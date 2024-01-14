28450268bd92dc27aa01a5fe34572c0956fc8862
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-01-14 14:50:01 +0100

    add more IL hooks now only FSMs remaining

--- AdjustedRNG.cs
+++ AdjustedRNG.cs
@@ -34,8 +34,76 @@ public class AdjustedRNG : SFCore.Generics.GlobalSettingsMod<RngSettings>
         cdr["RandomRangeInt"] = cdrt.GetMethod("RandomRangeInt", new Type[] { typeof(int), typeof(int), typeof(string) });
         cdr["RandomRange_int"] = cdrt.GetMethod("RandomRange", new Type[] { typeof(int), typeof(int), typeof(string) });
         cdr["RandomRange_float"] = cdrt.GetMethod("RandomRange", new Type[] { typeof(float), typeof(float), typeof(string) });
-            
-            
+
+        hooks.Add(new ILHook(typeof(Breakable).GetMethod("Break", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic),
+            il =>
+            {
+                ILCursor cursor = new ILCursor(il).Goto(0);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "Breakable.Break.Angle");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "Breakable.Break.Speed");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+            }));
+
+        hooks.Add(new ILHook(typeof(Breakable.FlingObject).GetMethod("Fling", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic),
+            il =>
+            {
+                ILCursor cursor = new ILCursor(il).Goto(0);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "Breakable.FlingObject.Fling.Amount");
+                cursor.Emit(OpCodes.Call, cdr["Range_int"]);
+            }));
+
+        hooks.Add(new ILHook(typeof(BreakableObject.FlingObject).GetMethod("Fling", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic),
+            il =>
+            {
+                ILCursor cursor = new ILCursor(il).Goto(0);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "BreakableObject.FlingObject.Fling.Amount");
+                cursor.Emit(OpCodes.Call, cdr["Range_int"]);
+            }));
+
+        hooks.Add(new ILHook(typeof(CrystalPieceSize).GetMethod("OnEnable", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic),
+            il =>
+            {
+                ILCursor cursor = new ILCursor(il).Goto(0);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "CrystalPieceSize.OnEnable.SizeSmall");
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "CrystalPieceSize.OnEnable.SizeBig");
+                cursor.Emit(OpCodes.Call, cdr["Range_int"]);
+            }));
+
         hooks.Add(new ILHook(typeof(EnemyBullet).GetMethod("OnEnable", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic),
             il =>
             {
@@ -129,6 +197,19 @@ public class AdjustedRNG : SFCore.Generics.GlobalSettingsMod<RngSettings>
                 cursor.Emit(OpCodes.Call, cdr["Range_float"]);
             }));
 
+        hooks.Add(new ILHook(typeof(GameManager).GetMethod("TimePasses", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic),
+            il =>
+            {
+                ILCursor cursor = new ILCursor(il).Goto(0);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "GameManager.TimePasses.BrettaPosition");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+            }));
+
         /*hooks.Add(new ILHook(typeof(GeoControl).GetMethod("Getter", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic),
             il =>
             {
@@ -184,6 +265,89 @@ public class AdjustedRNG : SFCore.Generics.GlobalSettingsMod<RngSettings>
                 }
             }));
 
+        hooks.Add(new ILHook(typeof(KnightHatchling).GetMethod("DoBuzz", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic),
+            il =>
+            {
+                ILCursor cursor = new ILCursor(il).Goto(0);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "KnightHatchling.DoBuzz.waitTime.outsideOfMinusYRoamingRange");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "KnightHatchling.DoBuzz.waitTime.outsideOfPlusYRoamingRange");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "KnightHatchling.DoBuzz.waitTime.outsideOfMinusXRoamingRange");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "KnightHatchling.DoBuzz.waitTime.outsideOfPlusXRoamingRange");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "KnightHatchling.DoBuzz.accelerationY.outsideOfMinusRoamingRange");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "KnightHatchling.DoBuzz.accelerationY.outsideOfPlusRoamingRange");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "KnightHatchling.DoBuzz.accelerationY.insideOfRoamingRange");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "KnightHatchling.DoBuzz.accelerationX.outsideOfMinusRoamingRange");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "KnightHatchling.DoBuzz.accelerationX.outsideOfPlusRoamingRange");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "KnightHatchling.DoBuzz.accelerationX.insideOfRoamingRange");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "KnightHatchling.DoBuzz.waitTime.insideOfRoamingRange");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+            }));
+
         /*hooks.Add(new ILHook(typeof(MegaJellyZap).GetMethod("MultiZapSequence", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic),
             il =>
             {
@@ -228,6 +392,39 @@ public class AdjustedRNG : SFCore.Generics.GlobalSettingsMod<RngSettings>
                 );
                 cursor.Remove();
                 cursor.Emit(OpCodes.Ldstr, "ScuttlerControl.Size");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "ScuttlerControl.MaxSpeed");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+            }));
+
+        hooks.Add(new ILHook(typeof(SetWalkerFacing).GetMethod("Apply", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic),
+            il =>
+            {
+                ILCursor cursor = new ILCursor(il).Goto(0);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "SetWalkerFacing.RandomStartDirection");
+                cursor.Emit(OpCodes.Call, cdr["Range_int"]);
+            }));
+
+        hooks.Add(new ILHook(typeof(SoulOrb).GetMethod("OnEnable", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic),
+            il =>
+            {
+                ILCursor cursor = new ILCursor(il).Goto(0);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "SoulOrb.ScaleModifier");
                 cursor.Emit(OpCodes.Call, cdr["Range_float"]);
             }));
 
@@ -285,7 +482,7 @@ public class AdjustedRNG : SFCore.Generics.GlobalSettingsMod<RngSettings>
                 cursor.Emit(OpCodes.Call, cdr["Range_float"]);
             }));
 
-        /*hooks.Add(new ILHook(typeof(FakeBat).GetMethod("Start", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic),
+        hooks.Add(new ILHook(typeof(SpellGetOrb).GetMethod("Start", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic),
             il =>
             {
                 ILCursor cursor = new ILCursor(il).Goto(0);
@@ -294,11 +491,39 @@ public class AdjustedRNG : SFCore.Generics.GlobalSettingsMod<RngSettings>
                     i => i.MatchCall(urt, "Range")
                 );
                 cursor.Remove();
-                cursor.Emit(OpCodes.Ldstr, "FakeBat.Start");
+                cursor.Emit(OpCodes.Ldstr, "SpellGetOrb.StartX");
                 cursor.Emit(OpCodes.Call, cdr["Range_float"]);
-            }));*/
-        
-        /*hooks.Add(new ILHook(typeof(FakeBat).GetMethod("Start", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic),
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "SpellGetOrb.StartY");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "SpellGetOrb.StartZ");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "SpellGetOrb.IdleTime");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "SpellGetOrb.Size");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+            }));
+
+        hooks.Add(new ILHook(typeof(SpellGetOrb).GetMethod("OnEnable", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic),
             il =>
             {
                 ILCursor cursor = new ILCursor(il).Goto(0);
@@ -307,9 +532,88 @@ public class AdjustedRNG : SFCore.Generics.GlobalSettingsMod<RngSettings>
                     i => i.MatchCall(urt, "Range")
                 );
                 cursor.Remove();
-                cursor.Emit(OpCodes.Ldstr, "FakeBat.Start");
+                cursor.Emit(OpCodes.Ldstr, "SpellGetOrb.Speed");
                 cursor.Emit(OpCodes.Call, cdr["Range_float"]);
-            }));*/
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "SpellGetOrb.Direction");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+            }));
+
+        hooks.Add(new ILHook(typeof(Walker).GetMethod("BeginStopped", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic),
+            il =>
+            {
+                ILCursor cursor = new ILCursor(il).Goto(0);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "Walker.TimeBeforeWalking");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+            }));
+
+        hooks.Add(new ILHook(typeof(Walker).GetMethod("BeginWalking", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic),
+            il =>
+            {
+                ILCursor cursor = new ILCursor(il).Goto(0);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "Walker.TimeBeforeStopping");
+                cursor.Emit(OpCodes.Call, cdr["Range_float"]);
+            }));
+
+        hooks.Add(new ILHook(typeof(Walker).GetMethod("EndStopping", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic),
+            il =>
+            {
+                ILCursor cursor = new ILCursor(il).Goto(0);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "Walker.StartToDirection");
+                cursor.Emit(OpCodes.Call, cdr["Range_Int"]);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "Walker.Turn");
+                cursor.Emit(OpCodes.Call, cdr["Range_Int"]);
+            }));
+
+        hooks.Add(new ILHook(typeof(Walker).GetMethod("StartMoving", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic),
+            il =>
+            {
+                ILCursor cursor = new ILCursor(il).Goto(0);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "Walker.DoTurnBeforeMoving");
+                cursor.Emit(OpCodes.Call, cdr["Range_Int"]);
+            }));
+
+        hooks.Add(new ILHook(typeof(WeaverlingEnemyList).GetMethod("GetTarget", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic),
+            il =>
+            {
+                ILCursor cursor = new ILCursor(il).Goto(0);
+                cursor.GotoNext
+                (
+                    i => i.MatchCall(urt, "Range")
+                );
+                cursor.Remove();
+                cursor.Emit(OpCodes.Ldstr, "WeaverlingEnemyList.RandomEnemyIndex");
+                cursor.Emit(OpCodes.Call, cdr["Range_Int"]);
+            }));
     }
 
     public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
@@ -321,6 +625,11 @@ public class AdjustedRNG : SFCore.Generics.GlobalSettingsMod<RngSettings>
         foreach ((string context, ContextValue value) in GlobalSettings.Contexts)
         {
             ContextDependendRandom.AddContext(context, value);
+        }
+
+        foreach (ILHook ilHook in hooks)
+        {
+            ilHook.Apply();
         }
 
         Log("Initialized");
